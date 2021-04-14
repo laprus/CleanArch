141dab8f11591c49f24f0d8a3e5658ae78187411
plaprus <pawel.laprus@hyland.com>
2021-04-14 20:46:41 +0200

    commit implementation of service and repository getCourses methods

---
+++ CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,27 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+
+        private UniversityDBContext _ctx;
+
+        public CourseRepository(UniversityDBContext context)
+        {
+            _ctx = context;
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            return _ctx.Courses;
+        }
+    }
+}
